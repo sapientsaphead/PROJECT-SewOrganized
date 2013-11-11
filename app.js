05b5e755e28ca8ece0eaf7ab996e80eccd6d11ff
Debra Zapata <debra.zapata@gmail.com>
2013-11-10 17:09:52 -0700

    Updates to Patterns

--- app.js
+++ app.js
@@ -67,36 +67,6 @@ app.get('/dummyuser', function(req, res){
 	});
 });
 
-app.post('/addpattern', function(req, res){
-	var pattern = req.body;
-
-	var newPattern = new Pattern({
-		username: 'unicorn',
-		company: pattern.company,
-		desc: pattern.name,
-		id: pattern.id,
-		url: pattern.url,
-		size: pattern.size,
-		imageurl: pattern.imageurl
-	});
-	newPattern.save(function(err){
-		if(err) {
-			res.send(500, 'Error encountered attempting to save new pattern to database.');
-		}
-		else {
-			res.send(pattern);
-		}
-	});
-});
-
-app.post('/editpattern', function(req, res) {
-	//get pattern from database
-	var id = req.body;
-	Pattern.findOne({_id: id}, function(err, pattern){
-		res.send(pattern);
-	})
-})
-
 app.get('/profile/:username', function(req, res){
 	var username = req.params.username;
 	var activeuser = User.findOne({username: username}, function (err, user) {
@@ -119,6 +89,67 @@ app.get('/patterns', function(req, res){
 	//get patterns from database
 	Pattern.find({}, function(err, patterns){
 		res.render('patterns', {patterns: patterns});
+	});
+});
+
+app.post('/addpattern', function(req, res){
+	var pattern = req.body;
+
+	var newPattern = new Pattern({
+		username: 'unicorn',
+		company: pattern.company,
+		desc: pattern.desc,
+		id: pattern.id,
+		url: pattern.url,
+		size: pattern.size,
+		imageurl: pattern.imageurl
+	});
+	newPattern.save(function(err){
+		if(err) {
+			res.send(500, 'Error encountered attempting to save new pattern to database.');
+		}
+		else {
+			res.send(pattern);
+		}
+	});
+});
+
+app.post('/activepattern', function(req, res) {
+	//get pattern from database
+	var id = req.body.objectId;
+	var activePattern = Pattern.findOne({_id: id}, function (err, pattern){
+		if(err) {
+			res.send('There was an error with your request.')
+		}
+		else {
+			res.send(pattern);
+		}
+	});
+});
+
+app.post('/editpattern', function(req, res) {
+	//for n number of arguments it loops through each to change the corresponding key value pair in db
+	var id = req.body.objectId;
+	//will need the id to modify just the one element
+	var activePattern = Pattern.update({_id: id}, function (err, data){
+		if(err) {
+			res.send('There was an error with your request.')
+		}
+		else {
+			res.send('Changes were saved successfully.');
+		}
+	});
+});
+
+app.post('/deletepattern', function(req, res) {
+	var id = req.body.objectId;
+	var deletePattern = Pattern.remove({_id: id}, function (err, data){
+		if(err) {
+			res.send('Unable to delete pattern at this time.')
+		}
+		else {
+			res.send(id);
+		}
 	});
 });
 
--- public/javascripts/patterns.js
+++ public/javascripts/patterns.js
@@ -37,25 +37,54 @@ $(document).ready(function(){
 		});
 	});
 
+	//should empty the pattern form input fields BUT DOES NOT!!!!
+	$(document).on('click', '.closeButton', function(){
+
+		$(this).closest('div.modal-content').find('input').each(function(){
+			console.log('this placeholder', $(this).find('placeholder'));
+			$(this).find('text').val('');
+			$(this).find('placeholder').remove();
+
+			//maybe try:
+			$(this).closest('form').find("input[type=text], textarea").val("");
+			// console.log('empty input', $(this).each('input').empty());
+		});
+	});
+
 	// Edit or delete an existing pattern
 	$(document).on('click', '.pattern', function(e){
 		e.preventDefault();
+		var objectId = $(this).attr('data-attr');
+	
+		$.post('/activepattern', {objectId: objectId}, function(pattern){
+			//include error handling here
+			var activePattern = pattern;
+			$('input:text').each(function(){
+				var keyname = $(this).attr('name');
+				$(this).attr('placeholder', activePattern[keyname]);
+			});
+			//delete pattern
+			$('.patternForm').on('click', '.deleteButton', function(e) {
+				e.preventDefault();
+				var response = confirm('Are you sure you want to remove this pattern from your library?');
+				if (response == true) {
+					// pattern id gets sent to server to remove from db
+					$.post('/deletepattern', {objectId: objectId}, function(id){
+						// remove deleted pattern from DOM (or should)
+						$('div').find("[data-attr='"+id+"']").remove();
+					});
+				}
+				else {
+					console.log('Request canceled.');
+				}	
+			});
+		});
+
 
 		$('.patternForm').on('click', '.submitButton',function(e){
 			e.preventDefault();
-			var that = $(this).closest('div.modal-content').find('form.newPattern');
-			var company = that.find('.company').val();
-			var desc = that.find('.desc').val();
-			var size = that.find('.size').val();
-			var id = that.find('.id').val();
-			var url = that.find('.url').val();
-			var imageurl = that.find('.imageurl').val();
-			var newpattern = {company: company, desc: desc, size: size, id: id, url: url, imageurl: imageurl};
 
-			$.post('/addpattern', newpattern, function(pattern){
-				//include error handling here
-				$allpatterns.append(inputTemplate(newpattern));
-			});
+			
 		});
 	});
 	
