8896fcc042c73e6d582dbaf4d83c6d3746bd4da3
Debra Zapata <debra.zapata@gmail.com>
2013-11-13 16:15:35 -0700

    Bad Wolf

--- public/javascripts/pinterest.js
+++ public/javascripts/pinterest.js
@@ -1,21 +1,5 @@
 $(document).ready(function(){
-	var url = "http://www.craftsy.com/classes/sewing/new"
-
-    $.ajax({
-        url: url,
-        type: "get",
-        dataType: "",
-        success: function(data) {
-        	// load the response into jquery element
-            // form tags are needed to get the entire html,head and body
-            $foop = $('<form>' + data.responseText + '</form>');
-        	// find contents of divs
-            $.each($foop.find('div.classCard'), function (idx, item) {
-                mytext = $(item).children().remove().text();
-                $('<div>'+mytext+'</div>').appendTo($('div.classes'));
-            });
-
-        }
-     });
+	
+    
 }); //end of document ready
 
